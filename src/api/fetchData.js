98b77d674df2e7bf0adca67dc3f57c5076df5548
firmankhoiril <firmankhoiril13@gmail.com>
2023-02-18 19:53:56 +0700

    change all

---
+++ src/api/fetchData.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const base_url = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  method: "GET",
+
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_APP_KEY_API,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchDataYoutube = async (url) => {
+  const data = await axios(`${base_url}/${url}`, options);
+  return data;
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { Stack, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
-import logo from "../utils/logo.png";
-import SearchBar from "./SearchBar";
 
-const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position: "sticky", backgroundColor: "#111827", justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
-      <Typography sx={{ color: "white", fontWeight: "900", fontSize: "18px", height: "20px", marginLeft: "5px" }}>YouClone</Typography>
-    </Link>
-    <SearchBar />
-  </Stack>
-);
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
 
 export default Navbar;
