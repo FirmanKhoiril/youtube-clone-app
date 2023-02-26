22605047bfc59777a572a947cced749a329cd4a9
firmankhoiril <firmankhoiril13@gmail.com>
2023-02-26 11:19:09 +0700

    error conditional done

--- src/components/conditonal/Error.jsx
+++ src/components/conditonal/Error.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Error = () => {
-  return <div>Error</div>;
+const Error = ({ error }) => {
+  return (
+    <div className="h-screen w-full space-x-2 items-center flex justify-center">
+      <span className="text-red-400"> {error.message}</span>
+      <span> which means its too many Request to server :)</span>
+    </div>
+  );
 };
 
 export default Error;
